1d94996bd9638edf2848491c34e03ad456c70f60
angermii <gornostaeva.tan@gmail.com>
2026-07-13 17:35:28 +0300

    2 коммит, исправила ошибки

--- tests/constructor.pl.tsx
+++ tests/constructor.pl.tsx
@@ -7,9 +7,11 @@ async function addIngredients(page: Page) {
   // нажатие кнопки добавить на булке
   await bunElement.getByRole('button', { name: /добавить/i }).click();
 
+  const constructor = page.getByTestId('burger-constructor');
   // проверяем ее в конструкторе
-  const bunTop = page.getByText(`${bun} (верх)`);
-  const bunBottom = page.getByText(`${bun} (низ)`);
+  const bunTop = constructor.getByText(`${bun} (верх)`);
+  const bunBottom = constructor.getByText(`${bun} (низ)`);
+
   await expect(bunTop).toBeVisible();
   await expect(bunBottom).toBeVisible();
 
@@ -21,49 +23,8 @@ async function addIngredients(page: Page) {
   await ingredientElement.getByRole('button', { name: /добавить/i }).click();
 
   // проверяем в конструкторе
-  await expect(
-    page.locator(`.constructor-element:has-text("${ingredient}")`)
-  ).toBeVisible();
+  await expect(constructor.getByText(`${ingredient}`)).toBeVisible();
 }
-
-test('HAR-файл пользователя', async ({ page }) => {
-  const responce = await page.request.post(
-    'https://norma.education-services.ru/api/auth/login',
-    {
-      data: {
-        email: 'user@test.com',
-        password: '111111'
-      }
-    }
-  );
-
-  const loginData = await responce.json();
-
-  // Извлекаем токены из ответа
-  const accessToken = loginData.accessToken;
-  const refreshToken = loginData.refreshToken;
-
-  await page.addInitScript(
-    ({ accessToken, refreshToken }) => {
-      document.cookie = `accessToken=${accessToken}; path=/`;
-      localStorage.setItem('refreshToken', refreshToken);
-    },
-    { accessToken, refreshToken }
-  );
-
-  await page.routeFromHAR('./tests/hars/users.har', {
-    url: '**/auth/user',
-    update: false
-  });
-
-  const responcePromise = page.waitForResponse((responce) =>
-    responce.url().includes('/auth/user')
-  );
-
-  await page.goto('/');
-
-  await responcePromise;
-});
 
 test('HAR-файл ингредиентов', async ({ page }) => {
   await page.routeFromHAR('./tests/hars/ingredients.har', {
@@ -94,9 +55,7 @@ test.describe('Создание заказа с пользователем и и
     await bunElement.click();
 
     // модальное окно открылось
-    const modal = page
-      .locator('h3:has-text("Ingredient Details")')
-      .locator('../..');
+    const modal = page.getByTestId('modal');
     await expect(modal).toBeVisible();
     await expect(modal).toContainText(bun);
 
@@ -121,6 +80,11 @@ test.describe('Создание заказа с пользователем и и
     });
 
     // загрузка пользователя
+    await page.addInitScript(() => {
+      document.cookie = 'accessToken=Bearer test-access-token; path=/';
+      localStorage.setItem('refreshToken', 'test-refresh-token');
+    });
+
     await page.routeFromHAR('./tests/hars/users.har', {
       url: '**/auth/user'
     });
@@ -138,6 +102,11 @@ test.describe('Создание заказа с пользователем и и
     });
 
     // загрузка пользователя
+    await page.addInitScript(() => {
+      document.cookie = 'accessToken=Bearer test-access-token; path=/';
+      localStorage.setItem('refreshToken', 'test-refresh-token');
+    });
+
     await page.routeFromHAR('./tests/hars/users.har', {
       url: '**/auth/user'
     });
@@ -151,18 +120,23 @@ test.describe('Создание заказа с пользователем и и
     // open page
     await page.goto('/');
 
+    await expect(
+      page.getByRole('button', { name: /добавить/i }).first()
+    ).toBeVisible();
+
     await addIngredients(page);
 
     // оформляем заказ
     await page.getByRole('button', { name: /оформить заказ/i }).click();
 
     // модалка с заказом открылась
-    await expect(page.getByText('идентификатор заказа')).toBeVisible();
-    const modal = page.getByText('идентификатор заказа').locator('../..');
+    await expect(page.getByText('6395')).toBeVisible();
+    const modal = page.getByTestId('modal');
 
     // проверка очищение конструктора
-    await expect(page.getByText('Выберите начинку')).toBeVisible();
-    await expect(page.getByText('Выберите булки').first()).toBeVisible();
+    const constructor = page.getByTestId('burger-constructor');
+    await expect(constructor.getByText('Выберите начинку')).toBeVisible();
+    await expect(constructor.getByText('Выберите булки').first()).toBeVisible();
 
     // закрытие по кнопке
     await modal
